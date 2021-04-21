b45bad250a00adb1ca459bee49416be3f0b1a22c
BartlomiejPuka <bartlomiej.puka@gmail.com>
2021-04-21 15:21:56 +0200

    - refactoring

--- src/app/sphere-engine/sphere-engine.component.ts
+++ src/app/sphere-engine/sphere-engine.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterContentInit, AfterViewChecked, AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 
 // tslint:disable-next-line:typedef
 declare function drop_SE_widget(widgetId: any);
@@ -10,20 +10,29 @@ declare function load_SE_widget(widgetId: any);
   templateUrl: './sphere-engine.component.html',
   styleUrls: ['./sphere-engine.component.css']
 })
-export class SphereEngineComponent implements OnInit, OnDestroy {
+export class SphereEngineComponent implements OnInit, OnDestroy, AfterViewInit {
+  dataWidget: string;
   dataId: string;
+  seObj: any;
   constructor() {
     this.dataId = 'test-id';
+    this.dataWidget = 'tD2Ln2MoPz';
+    this.seObj = null;
   }
-
   ngOnInit(): void {
-    console.log('oninit', this.dataId);
-    load_SE_widget(this.dataId);
   }
-
   ngOnDestroy(): void {
     console.log('ondestroy', this.dataId);
     drop_SE_widget(this.dataId);
+  }
+  ngAfterViewInit(): void {
+    this.loadWidget();
+  }
+
+  loadWidget(): void {
+    console.log('load', this.dataId);
+    this.seObj = load_SE_widget(this.dataId);
+    console.log(this.seObj);
   }
 
 }
