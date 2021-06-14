54242b3031de1501a364e70d9317ef8ff73c3cfe
BartlomiejPuka <bartlomiej.puka@gmail.com>
2021-06-14 21:28:23 +0200

    wip

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css', './cart.image.b64.css']
 })
 export class CartComponent implements OnInit {
 
