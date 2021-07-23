e1945aca51acd4521642943dd87c23da2a38ffbf
BartlomiejPuka <bartlomiej.puka@gmail.com>
2021-07-23 19:15:19 +0200

    - refactor - added more payloads for course details - added cart notification service

---
+++ src/app/course/course.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {CourseDetailsPayload} from '../shared/course-details-payload';
+import {ApiHttpService} from '../backend-api/api-http.service';
+import {ApiEndpointsService} from '../backend-api/api-endpoints.service';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+
+  courseUrlSlug: string;
+  courseDetailsPayload: CourseDetailsPayload;
+  constructor(
+    private apiHttpService: ApiHttpService,
+    private apiEndpointService: ApiEndpointsService,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    // tslint:disable-next-line:radix
+    this.courseUrlSlug = this.route.snapshot.paramMap.get('slug');
+    this.fetchData();
+  }
+
+  fetchData(): void {
+    this.apiHttpService.get<CourseDetailsPayload>(this.apiEndpointService.getCourseDetailsByUrlSlug(this.courseUrlSlug))
+      .subscribe((data) => {
+        this.courseDetailsPayload = data;
+      });
+  }
+}
---
+++ src/app/shared-services/cart-notification-service/cart-notification.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {ApiHttpService} from '../../backend-api/api-http.service';
+import {ApiEndpointsService} from '../../backend-api/api-endpoints.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartNotificationService {
+  public cartItemCount: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  constructor(
+    private apiHttpService: ApiHttpService,
+    private apiEndpointService: ApiEndpointsService
+  ) { }
+  public refreshCartItemsCount(): void {
+    this.apiHttpService.get<number>(this.apiEndpointService.getCartItemsCount())
+      .subscribe(data => {
+        console.log(data);
+        this.cartItemCount.next(data);
+      });
+  }
+}
