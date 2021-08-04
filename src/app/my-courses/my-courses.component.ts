9e4285288eb150dfbf7866b85ee0f0bcac1b8a48
BartlomiejPuka <bartlomiej.puka@gmail.com>
2021-08-04 11:45:52 +0200

    - moved endpoints strings to separate services.

--- src/app/my-courses/my-courses.component.ts
+++ src/app/my-courses/my-courses.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {EnrolledCoursePayload} from '../shared/my-courses/enrolled-course-payload';
 import {ApiHttpService} from '../backend-api/api-http.service';
 import {ApiEndpointsService} from '../backend-api/api-endpoints.service';
+import {EnrolledCourseEndpointsApiService} from '../backend-api/enrolled-course-endpoints-api/enrolled-course-endpoints-api.service';
 
 @Component({
   selector: 'app-my-courses',
@@ -13,6 +14,7 @@ export class MyCoursesComponent implements OnInit {
   enrolledCourses: Array<EnrolledCoursePayload>;
   constructor(
     private apiHttpService: ApiHttpService,
+    private enrolledCourseEndpointsApiService: EnrolledCourseEndpointsApiService,
     private apiEndpointService: ApiEndpointsService,
   ) { }
 
@@ -21,7 +23,7 @@ export class MyCoursesComponent implements OnInit {
   }
 
   fetchData(): void {
-    this.apiHttpService.get<Array<EnrolledCoursePayload>>(this.apiEndpointService.getAllBoughtCourses())
+    this.apiHttpService.get<Array<EnrolledCoursePayload>>(this.enrolledCourseEndpointsApiService.getAllBoughtCourses())
       .subscribe(data => {
         this.enrolledCourses = data;
     });
