c79a2d24ab74add8e4db8f42a640b7c143678ed5
BartlomiejPuka <bartlomiej.puka@gmail.com>
2021-08-10 19:43:14 +0200

    improving config for deploy

--- src/app/backend-api/api-http.service.ts
+++ src/app/backend-api/api-http.service.ts
@@ -29,19 +29,10 @@ export class ApiHttpService {
     return this.http.get<T>(url, options);
   }
     // tslint:disable-next-line:typedef
-  public post<T>(url: string, data: any, options?: {
-    headers?: HttpHeaders | {
-      [header: string]: string | string[];
-    };
-    observe?: 'body';
-    params?: HttpParams | {
-      [param: string]: string | string[];
-    };
-    reportProgress?: boolean;
-    responseType?: 'json';
-    withCredentials?: boolean;
-  }) {
-    options = options ? options : this.constants.defaultOptions;
+  public post<T>(url: string, data: any, options?: any) {
+    if (!options){
+      options = this.constants.defaultOptions;
+    }
     return this.http.post<T>(url, data, options);
     // tslint:disable-next-line:typedef
   }  public put<T>(url: string, data: any, options?: any) {
